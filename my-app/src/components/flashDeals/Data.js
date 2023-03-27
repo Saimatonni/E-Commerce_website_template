6916e87c6dad3ef9434ad02b53bf60f5b80d0c45
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-03-27 11:40:50 +0600

    Add files via upload

---
+++ my-app/src/components/flashDeals/Data.js
@@ -0,0 +1,47 @@
+const Data = {
+    productItems: [
+      {
+        id: 1,
+        discount: 50,
+        cover: "./images/flash/flash-1.jpeg",
+        name: "Gift1",
+        price: 100,
+      },
+      {
+        id: 2,
+        discount: 40,
+        cover: "./images/flash/flash-2.jpeg",
+        name: "Gift2",
+        price: 20,
+      },
+      {
+        id: 3,
+        discount: 40,
+        cover: "./images/flash/flash-3.jpeg",
+        name: "Gift3",
+        price: 200,
+      },
+      {
+        id: 4,
+        discount: 40,
+        cover: "./images/flash/flash-4.jpeg",
+        name: "Gift4",
+        price: 50,
+      },
+      {
+        id: 5,
+        discount: 50,
+        cover: "./images/flash/flash-1.jpeg",
+        name: "Gift1",
+        price: 100,
+      },
+      {
+        id: 6,
+        discount: 50,
+        cover: "./images/flash/flash-3.jpeg",
+        name: "Gift2",
+        price: 100,
+      },
+    ],
+  }
+  export default Data
