1f62cbcfd5d270dd60a7767f1564f31d3d3a7c8f
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-03-27 12:07:58 +0600

    Add files via upload

---
+++ my-app/src/components/newarrivals/Ndata.js
@@ -0,0 +1,34 @@
+const Ndata = [
+    {
+      cover: "./images/arrivals/gift1.jpeg",
+      name: "Sunglass",
+      price: "150",
+    },
+    {
+      cover: "./images/arrivals/gift2.jpeg",
+      name: "Makeup",
+      price: "250",
+    },
+    {
+      cover: "./images/arrivals/gift3.jpeg",
+      name: "Smart Watch",
+      price: "50",
+    },
+    {
+      cover: "./images/arrivals/gift4.jpeg",
+      name: "Lipstick",
+      price: "15",
+    },
+    {
+      cover: "./images/arrivals/gift5.jpeg",
+      name: "Green Plant",
+      price: "10",
+    },
+    {
+      cover: "./images/arrivals/gift6.jpeg",
+      name: "Bonsai tree",
+      price: "400",
+    },
+  ]
+  
+  export default Ndata
