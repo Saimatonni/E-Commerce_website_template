09e8a668126f2d76a929903331fcb5ebdac070b9
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-03-27 10:58:03 +0600

    Add files via upload

---
+++ my-app/src/components/discount/Ddata.js
@@ -0,0 +1,33 @@
+const Ddata = [
+    {
+      cover: "./images/discount/gift1.jpeg",
+      name: "BenuX 2022",
+      price: "$250",
+    },
+    {
+      cover: "./images/discount/gift2.jpeg",
+      name: "Sony TV 1080p",
+      price: "$450",
+    },
+    {
+      cover: "./images/discount/gift3.jpeg",
+      name: "Sony PS4",
+      price: "$50",
+    },
+    {
+      cover: "./images/discount/gift4.jpeg",
+      name: "Setgearr 2022",
+      price: "$100",
+    },
+    {
+      cover: "./images/discount/gift5.jpeg",
+      name: "Tony BGB",
+      price: "$20",
+    },
+    {
+      cover: "./images/discount/gift6.jpeg",
+      name: "RG products",
+      price: "$200",
+    },
+  ]
+  export default Ddata
