e77d386da58c046705d491938f09fbc1362be36c
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-03-27 12:09:59 +0600

    Add files via upload

---
+++ my-app/src/components/top/Tdata.js
@@ -0,0 +1,29 @@
+const Tdata = [
+    {
+      cover: "./images/top/gift1.jpeg",
+      para: "Gift 1",
+      desc: "3k orders this week",
+    },
+    {
+      cover: "./images/top/gift2.jpeg",
+      para: "Gift 2",
+      desc: "4k orders this week",
+    },
+    {
+      cover: "./images/top/gift3.jpeg",
+      para: "Gift 3",
+      desc: "6k orders this week",
+    },
+    {
+      cover: "./images/top/gift4.jpeg",
+      para: "Gift 4",
+      desc: "4k orders this week",
+    },
+    {
+      cover: "./images/top/gift5.jpeg",
+      para: "Gift 5",
+      desc: "6k orders this week",
+    },
+  ]
+  
+  export default Tdata
